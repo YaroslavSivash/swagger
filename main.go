40ce1892cd3d795bf25b064a693d64dc1c0c25c6
yaroslav <sivash2401@gmail.com>
2021-10-20 15:42:16 +0300

    0.1

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/YaroslavSivash/swagger/docs"
+	_ "github.com/YaroslavSivash/swagger"
+
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
